81dd7a9495224eef46e260714a19234a6463e730
wskim <wskim@ubuntu20.myguest.virtualbox.org>
2023-05-09 23:25:48 +0900

    v0.1

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,8 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = ' b1840eb145b2dfe1a7bce12f53c1202d4f699dd0'
+#AESD_ASSIGNMENTS_VERSION = ' b1840eb145b2dfe1a7bce12f53c1202d4f699dd0'
+AESD_ASSIGNMENTS_VERSION =  'ec912273c60b1df071846ea0bc5ebda272f22516'  
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -23,6 +24,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket  
 endef
 
 $(eval $(generic-package))
