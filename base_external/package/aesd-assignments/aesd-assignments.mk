ccc52c070c29319c7946d66ea60c83059e5b4b14
wskim <wskim@ubuntu20.myguest.virtualbox.org>
2023-05-12 22:50:55 +0900

    v0.3

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,7 +7,9 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 #AESD_ASSIGNMENTS_VERSION = ' b1840eb145b2dfe1a7bce12f53c1202d4f699dd0'
-AESD_ASSIGNMENTS_VERSION =  'ec912273c60b1df071846ea0bc5ebda272f22516'  
+#AESD_ASSIGNMENTS_VERSION =  'ec912273c60b1df071846ea0bc5ebda272f22516'  
+AESD_ASSIGNMENTS_VERSION =  'c7dbb5564ba28d5c5d87d8bc755deb3f03de1072'  
+
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
